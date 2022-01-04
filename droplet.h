7668b865f4b6c3b7665ed6f743bdddc2b2d55474
josbrink <G00362643@gmit.ie>
2022-01-04 23:28:28 +0000

    working digital rain

--- droplet.h
+++ droplet.h
@@ -2,22 +2,33 @@
 #define DROPLET_H
 
 #include <vector>
+#include <iostream>
+#include <random>
+#include <windows.h>
 
 static int dropletCount = 0;
 
 class Droplet {
 public:
+	Droplet();
 	Droplet(int);
+	Droplet(int, int);
 	~Droplet();
-	static int getDropletCount();
-	int getDroplet();
+
+	void getDroplet();
+
 	int getLineLength();
 	int getPos();
+
+	static int getDropletCount();
+
 private:
+	HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+	std::vector<int> colourSet{ 1, 2, 3, 4, 5, 6 };
 	std::vector<int> numSet;
 	int lineLength = 0;
-	int lineCount = 0;
 	int pos = 0;
+	int charColour = 0;
 };
 
 #endif
